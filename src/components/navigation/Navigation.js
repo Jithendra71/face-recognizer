ab24ad9c2add3235ba61f4c73d88fb820a297078
Jithendra71 <jithendra4567890@gmail.com>
2021-07-05 18:14:43 +0530

    changing everything

---
+++ src/components/navigation/Navigation.js
@@ -0,0 +1,10 @@
+import React,{ Component } from 'react';
+
+class Navigation extends Component {
+render() {
+    return <h1 className = 'tr f3 link dim black underline pa3 pointer'>Sign Out</h1>
+}
+}
+
+
+export default Navigation;
